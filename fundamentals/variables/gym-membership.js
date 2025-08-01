7c868341e8bc1f1ca78fc3407f7d24abaf6d3a16
DevDereck <devdereck08@gmail.com>
2025-07-31 20:30:58 -0600

    chore(variables) : add gym-membership exercise

---
+++ fundamentals/variables/gym-membership.js
@@ -0,0 +1,24 @@
+// El ELite Fitness Center es un gym  boutique ubicado Mercedes Norte, Heredia , 
+// que ofrece membresías por $150 al mes.
+// que opera de 6AM a 8PM, emplea trabajadores certificados
+// y cuenta con comodidaddes como sauna, piscina y clases grupales.
+
+
+const gymName = 'ELite Fitness Center'
+const location = 'Mercedes Norte, Heredia'
+const priceMembership = '$150'
+const openingTime = '6AM'
+const closeTime = '8PM'
+
+const amenity1 = 'sauna'
+const amenity2 = 'piscina'
+const amenity3 = 'clases grupales'
+
+
+console.log(` 
+  Gym Name: ${gymName}
+  Location: ${location}
+  PriceMembership: ${priceMembership}
+  Hotel Amenitys: ${amenity1} - ${amenity2} - ${amenity3}\n
+  Schedule -> opening: ${openingTime} - close: ${closeTime}
+  `)
