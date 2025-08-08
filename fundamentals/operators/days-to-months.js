14dfb2f69afef411bf49f6e7ea2eacd807808f0c
DevDereck <devdereck08@gmail.com>
2025-08-07 18:29:58 -0600

    chore(operators): add days-to-months exercise

---
+++ fundamentals/operators/days-to-months.js
@@ -0,0 +1,25 @@
+/*
+Escribe un programa en JavaScript que solicite al usuario ingresar el
+número de días y calcule el número equivalente de meses y días restantes,
+considerando que un mes consta de 30 días.
+
+Este programa utiliza el módulo prompt-sync para recibir interactivamente
+la entrada del usuario. Después de calcular la cantidad de meses y días restantes
+según la entrada, el programa muestra un mensaje al usuario indicando el equivalente
+en meses y días restantes.
+
+Como pista, puedes usar Math.floor() para redondear el resultado a un número entero.
+
+Si el usuario ingresa 100, la salida será: 100 days are 3 months and 10 days.
+*/
+
+const prompt = require ('prompt-sync') ()
+
+const DAYS_IN_MONTH = 30
+
+const days = parseInt(prompt('¿How many days do you want to calculate?: '))
+
+const months = Math.floor(days / DAYS_IN_MONTH)
+const daysLeft = days % DAYS_IN_MONTH
+
+console.log(`${days} days are: ${months} months and ${daysLeft} days`) 
