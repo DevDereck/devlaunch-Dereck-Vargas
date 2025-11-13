de22c772a0eb6661e61ce4be48f96f617d1ea2fe
DevDereck <devdereck08@gmail.com>
2025-11-13 16:58:22 -0600

    feat(typescript): migrated objects folder file to ts extension

---
+++ object-oriented-programming/typescript/account.ts
@@ -0,0 +1,74 @@
+/*
+Estás diseñando un sistema básico para gestionar cuentas bancarias de clientes.
+
+Cada cuenta bancaria tendrá un identificador único, un nombre del titular, y un saldo.
+
+Tu tarea es crear una función createAccount que genere un objeto de cuenta bancaria.
+
+El objeto debe ser creado por medio de un function declaration, con todas las propiedades y
+métodos deben estar definidos dentro de la función usando this.
+
+Propiedades de la cuenta bancaria (dentro de la función usando this):
+
+id: identificador único de la cuenta.
+holderName: nombre del titular de la cuenta.
+balance: saldo de la cuenta.
+
+Métodos de la cuenta bancaria (dentro de la función usando this):
+
+deposit(amount): incrementa el saldo de la cuenta.
+withdraw(amount): disminuye el saldo de la cuenta si hay suficientes fondos.
+transfer(toAccount, amount): transfiere fondos de la cuenta actual a otra cuenta
+si hay suficientes fondos disponibles.
+
+*/
+
+
+interface AccountShape {
+  id : string
+  holderName : string
+  balance: number
+  deposit: (amount : number) => void
+  withdraw: (amount : number) => number | null
+  transfer: (toAccount: AccountShape, amount : number) => AccountShape | null 
+}
+
+const Account = function(id : string, holderName: string, balance  : number = 0) :AccountShape {
+  this.id = id;
+  this.holderName = holderName;
+  this.balance = balance;
+
+  this.deposit = (amount : number) => {
+    this.balance += amount;
+  };
+
+  this.withdraw = (amount : number) => {
+    if (this.balance >= amount) {
+      this.balance -= amount;
+      return amount;
+    }
+
+    return null;
+  };
+
+  this.transfer = (toAccount : AccountShape, amount : number) => {
+    const money = this.withdraw(amount);
+
+    if (money) {
+      toAccount.deposit(money);
+      return toAccount;
+    }
+
+    return null;
+  };
+
+  return this;
+}
+
+const acc1: AccountShape = new (Account as any)(1, "Dereck", 10000);
+acc1.deposit(1000);
+acc1.withdraw(5000);
+
+const acc2 : AccountShape = new (Account as any) (2, "Marifer");
+console.log(acc1.transfer(acc2, 5000));
+console.log(acc1);
--- object-oriented-programming/typescript/index.ts
+++ object-oriented-programming/typescript/index.ts
@@ -1,1 +1,33 @@
+let message : string = 'Hola Dereck'
+
+
 console.log('Hello World!!')
+
+
+function grades (a:number, b: number) : number {
+  return (a+b)
+}
+
+interface Account {
+  id: string | number
+  name: string
+  balance: number
+}
+
+const createAccount = (id: string | number, name: string, balance: number = 0) : Account => {
+  return {
+    id, 
+    name,
+    balance
+  }
+}
+
+const acc1 =  createAccount(1, 'Dereck', 0)
+acc1.balance
+
+import promptSync from 'prompt-sync'
+
+const prompt = promptSync ();
+
+const result = prompt ('message: ') 
+console.log(result)
